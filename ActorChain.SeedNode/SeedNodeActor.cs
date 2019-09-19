b5d06f10f8b4ab7c81845ae259842564e886e236
iDormenco <idormenco@gmail.com>
2019-09-19 22:11:51 +0300

    Update SeedNodeActor.cs

--- ActorChain.SeedNode/SeedNodeActor.cs
+++ ActorChain.SeedNode/SeedNodeActor.cs
@@ -6,11 +6,18 @@ using Akka.Actor;
 
 namespace ActorChain.SeedNode
 {
-	public class SeedNodeActor : ReceiveActor, IHandle<AddTransactionMessage>, IHandle<GetTransactionsMessage>, IHandle<ClearTransactionsMessage>, IHandle<GetLastTransactionHashMessage>
+	public class SeedNodeActor : ReceiveActor
+	, IHandle<AddTransactionMessage>
+	, IHandle<GetTransactionsMessage>
+	, IHandle<ClearTransactionsMessage>
+	, IHandle<GetLastTransactionHashMessage>
+	, IHandle<GetBlockChainMessage>
+	, IHandle<RegisterBlockMessage>
 	{
 		private readonly ActorSelection _system = Context.ActorSelection("akka.tcp://ActorCoinNetwork@localhost:8081/user/SystemSupervisor");
 		private readonly Queue<Transaction> _transactions = new Queue<Transaction>();
-		private string LastBlockHash = string.Empty;
+		private readonly List<Block> _blockchain = new List<Block>();
+		private string lastBlockHash = string.Empty;
 
 		public SeedNodeActor()
 		{
@@ -18,6 +25,8 @@ namespace ActorChain.SeedNode
 			Receive<GetTransactionsMessage>(Handle);
 			Receive<ClearTransactionsMessage>(Handle);
 			Receive<GetLastTransactionHashMessage>(Handle);
+			Receive<GetBlockChainMessage>(Handle);
+			Receive<RegisterBlockMessage>(Handle);
 		}
 		public void Handle(AddTransactionMessage message)
 		{
@@ -38,7 +47,25 @@ namespace ActorChain.SeedNode
 
 		public void Handle(GetLastTransactionHashMessage message)
 		{
-			Sender.Tell(LastBlockHash);
+			Sender.Tell(lastBlockHash);
 		}
-	}
+
+        public void Handle(GetBlockChainMessage message)
+        {
+            Sender.Tell(new GetBlockChainResponseMessage(_blockchain.AsReadOnly()));
+        }
+
+        public void Handle(RegisterBlockMessage message)
+        {
+            _blockchain.Add(new Block(){
+				Transactions = message.Transactions,
+				Hash = message.Hash,
+				Nonce = message.Nonce,
+				Timestamp= message.Timestamp,
+				PreviousBlockHash = message.PreviousBlockHash
+			});
+
+			lastBlockHash = message.Hash;
+        }
+    }
 }
