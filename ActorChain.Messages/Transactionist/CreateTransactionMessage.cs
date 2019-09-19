b558524d098750c27c227375c65e81b79c0b175d
Ion Dormenco <idormenco@gmail.com>
2019-09-19 12:46:17 +0300

    Update CreateTransactionMessage.cs

--- ActorChain.Messages/Transactionist/CreateTransactionMessage.cs
+++ ActorChain.Messages/Transactionist/CreateTransactionMessage.cs
@@ -2,6 +2,15 @@
 {
 	public class CreateTransactionMessage
 	{
-		
+		public string Sender { get; }
+		public string Receiver { get; }
+		public decimal Amount { get; }
+
+		public CreateTransactionMessage(string sender, string receiver, decimal amount)
+		{
+			Sender = sender;
+			Receiver = receiver;
+			Amount = amount;
+		}
 	}
 }
