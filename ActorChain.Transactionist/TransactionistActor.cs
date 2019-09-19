f6d3db47093e98f299a9905d98175c16b2a91c2b
iDormenco <idormenco@gmail.com>
2019-09-19 21:26:21 +0300

    transactionist message handling

--- ActorChain.Transactionist/TransactionistActor.cs
+++ ActorChain.Transactionist/TransactionistActor.cs
@@ -1,21 +1,18 @@
-﻿using ActorChain.Messages.Transactionist;
+﻿using ActorChain.Messages.SeedNodeMessages;
+using ActorChain.Messages.Transactionist;
 using Akka.Actor;
 
-namespace ActorChain.Transactionist
-{
-	public class TransactionistActor : ReceiveActor, IHandle<CreateTransactionMessage>
-	{
-		private readonly ActorSelection _system = Context.ActorSelection("akka.tcp://ActorCoinNetwork@localhost:8081/user/SystemSupervisor");
+namespace ActorChain.Transactionist {
+	public class TransactionistActor : ReceiveActor, IHandle<CreateTransactionMessage> {
+		private readonly ActorSelection _system = Context.ActorSelection ("akka.tcp://ActorCoinNetwork@localhost:8081/user/SystemSupervisor");
+		private readonly ActorSelection _seedNode = Context.ActorSelection ("akka.tcp://SeedNodeSystem@localhost:8082/user/SeedNode");
 
-
-		public TransactionistActor()
-		{
-			Receive<CreateTransactionMessage>(Handle);
+		public TransactionistActor () {
+			Receive<CreateTransactionMessage> (Handle);
 		}
 
-		public void Handle(CreateTransactionMessage message)
-		{
-			throw new System.NotImplementedException();
+		public void Handle (CreateTransactionMessage message) {
+			_seedNode.Tell (new AddTransactionMessage (message.Sender, message.Receiver, message.Amount));
 		}
 	}
 }
