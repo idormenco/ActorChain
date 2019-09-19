dcbdc2c67ff0adf23fd4700ba872e7ce83d0ce93
Ion Dormenco <idormenco@gmail.com>
2019-09-19 12:02:16 +0300

    add transactionist actor

---
+++ ActorChain.Transactionist/TransactionistActor.cs
@@ -0,0 +1,15 @@
+﻿using Akka.Actor;
+
+namespace ActorChain.Transactionist
+{
+	public class TransactionistActor : ReceiveActor
+	{
+		private readonly ActorSelection _system = Context.ActorSelection("akka.tcp://ActorCoinNetwork@localhost:8081/user/SystemSupervisor");
+
+
+		public TransactionistActor()
+		{
+			Receive<object>(m => _system.Tell(m));
+		}
+	}
+}
