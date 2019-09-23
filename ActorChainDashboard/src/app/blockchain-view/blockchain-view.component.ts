33cafdfddde31ba808d52559963eeddc4a7f70a2
iDormenco <idormenco@gmail.com>
2019-09-23 19:33:28 +0300

    dashboard view blockchain , view pending transactions

--- ActorChainDashboard/src/app/blockchain-view/blockchain-view.component.ts
+++ ActorChainDashboard/src/app/blockchain-view/blockchain-view.component.ts
@@ -76,7 +76,583 @@ export class BlockchainViewComponent implements OnInit {
             sender: 'ion3'
           },
         ]
-      }
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
+      {
+        hash: 'hash2',
+        timestamp: 13333,
+        nonce: 1,
+        previousHash: 'hash1',
+        transactions: [
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+          {
+            amount: 1,
+            receiver: 'ion',
+            sender: 'ion2'
+          },
+          {
+            amount: 2,
+            receiver: 'ion',
+            sender: 'ion3'
+          },
+        ]
+      },
     ];
   }
 
--- ActorChainDashboard/src/app/transaction-view/transaction-view.component.ts
+++ ActorChainDashboard/src/app/transaction-view/transaction-view.component.ts
@@ -1,3 +1,4 @@
+import { Transaction } from './../blockchain-view/blockchain-view.component';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction-view.component.scss']
 })
 export class TransactionViewComponent implements OnInit {
-
+  transactions: Transaction[] = [
+    {
+      sender: 'ion',
+      receiver: 'vasile',
+      amount: 7
+    },
+    {
+      sender: 'ion',
+      receiver: 'vasile',
+      amount: 6
+    },
+    {
+      sender: 'ion',
+      receiver: 'vasile',
+      amount: 5
+    },
+    {
+      sender: 'ion',
+      receiver: 'vasile',
+      amount: 4
+    },
+    {
+      sender: 'ion',
+      receiver: 'vasile',
+      amount: 3
+    },
+  ];
   constructor() { }
 
   ngOnInit() {
